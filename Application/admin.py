34cb93e78aed0c60011af702082703c30e36e457
deajadrovska <jadrovska.dea@gmail.com>
2025-05-26 11:27:22 +0200

    Added models and registered them in the Admin panel

--- Application/admin.py
+++ Application/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from Application.models import RealEstate, Agent, Characteristic, CharacteristicRealEstate, AgentRealEstate
+
 # Register your models here.
+
+
+
+admin.site.register(RealEstate)
+admin.site.register(Agent)
+admin.site.register(Characteristic)
+admin.site.register(CharacteristicRealEstate)
+admin.site.register(AgentRealEstate)
+
--- Application/models.py
+++ Application/models.py
@@ -1,3 +1,51 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+
+#PROPERTY
+class RealEstate(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    area = models.FloatField()
+    date = models.DateField()
+    image = models.ImageField(upload_to='images/')
+    reserved = models.BooleanField()
+    sold = models.BooleanField()
+
+    def __str__(self):
+        return f'{self.name} - {self.area} - {self.sold}'
+
+class Agent(models.Model):
+    name = models.CharField(max_length=100)
+    contact_phone = models.CharField(max_length=100)
+    profile_url = models.CharField(max_length=100)
+    total_sales = models.IntegerField()
+    email = models.EmailField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.name} - {self.email}'
+
+#AGENT - PROPERTY
+class AgentRealEstate(models.Model):
+    realestate = models.ForeignKey(RealEstate, on_delete=models.CASCADE)
+    agent = models.ForeignKey(Agent, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.realestate.name} - {self.agent.name}'
+
+class Characteristic(models.Model):
+    name = models.CharField(max_length=100)
+    value = models.FloatField()
+
+    def __str__(self):
+        return f'{self.name} - {self.value}'
+
+class CharacteristicRealEstate(models.Model):
+    characteristic = models.ForeignKey(Characteristic, on_delete=models.CASCADE)
+    realestate = models.ForeignKey(RealEstate, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.characteristic.name} - {self.realestate.name}'
